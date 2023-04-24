80cf5a80c83fe57563facf8c4a37ff70be3a5977
Avinash Anand <avinashmgr53@gmail.com>
2023-04-25 05:07:29 +0530

    commit change

---
+++ src/components/MainDash/MainDash.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MainDash.css'
+import Table from '../Table/Table'
+import Cards from '../Cards/Cards'
+
+const MainDash = () => {
+  return (
+    <div className='MainDash'>
+        <h1>Dashboard</h1>
+        <Cards/>
+        <Table/>
+    </div>
+  )
+}
+
+export default MainDash
